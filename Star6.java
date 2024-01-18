116a99bd34ebc966324fcb00d393c04e33ed341e
Tusharlohave <tusharlohawe140@gmial.com>
2024-01-18 19:04:32 +0530

    Error Solved

--- Star6.java
+++ Star6.java
@@ -1,3 +1,11 @@
+// Write a java program to print the following pattern:
+//       *
+//     * *
+//   * * *
+// * * * *
+//   * * *
+//     * *
+//       *
 public class Star6 {
     public static void main(String args[]){
         for(int i=1;i<=4;i++){
