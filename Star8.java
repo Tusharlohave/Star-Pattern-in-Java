68ec5d60e820c2e39035de42d315e7cf1ffa09d0
Tusharlohave <tusharlohawe140@gmial.com>
2024-01-19 19:31:42 +0530

    Add Eithth Star Pattern

---
+++ Star8.java
@@ -0,0 +1,27 @@
+// Write a java program to print the following star pattern
+//    * 
+//   *** 
+//  ***** 
+// ******* 
+
+
+
+public class Star8 {
+    public static void main(String args[]){
+        for(int i=0;i<=4;i++){
+            for(int j=3;j>=i;j--){
+                System.out.print(" ");
+            }
+
+            for(int k=1;k<=i;k++){
+                System.out.print("*");
+            }
+            
+            for(int l=2;l<=i;l++){
+                System.out.print("*");
+            }
+
+            System.out.println(" ");
+        }
+    }
+}
