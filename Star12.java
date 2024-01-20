26cb4907407b3c3a2dca8905afc975118d391b51
Tusharlohave <tusharlohawe140@gmial.com>
2024-01-20 18:26:30 +0530

    Add Twelth Star Pattern

---
+++ Star12.java
@@ -0,0 +1,23 @@
+// Write a java program to print following pattern:
+//      * 
+//    *   * 
+//  *      * 
+//*         *
+public class Star12 {
+    public static void main(String args[]){
+            for(int i=1;i<=4;i++){
+                for(int j=4;j>=i;j--){
+                    System.out.print(" ");
+                }
+                for(int k=1;k<(i*2);k++){
+                    if(k>1 && k<(i*2)-1){
+                        System.out.print(" ");
+                    }
+                    else{
+                        System.out.print("*");
+                    }
+                }
+                System.out.println(" ");
+            }
+    }
+}
